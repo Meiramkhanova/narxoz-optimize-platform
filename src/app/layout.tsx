0daf6deb01789fa01319a0ab05ecdccc1ca86df9
adinamei <adinaone04@gmail.com>
2025-12-25 14:34:01 +0500

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-icon.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
